5c172eec453ae8435e99d98a6f63e8c5179f7de5
Derek Dao <147881128+DD00DD@users.noreply.github.com>
2025-12-19 15:22:34 -0500

    Add files via upload

---
+++ movie_kb.pl
@@ -0,0 +1,146 @@
+% --- Release information ---
+releaseInfo(unforgiven, 1992, 130).
+releaseInfo(gran_torino, 2008, 116).
+releaseInfo(minority_report, 2002, 145).
+releaseInfo(superman_returns, 2006, 154).
+releaseInfo(jaws, 1975, 124).
+releaseInfo(mad_max_fury_road, 2015, 120).
+releaseInfo(300, 2006, 117).
+releaseInfo(alien_covenant, 2017, 122).
+releaseInfo(atomic_blonde, 2017, 115).
+releaseInfo(it, 2017, 190).
+releaseInfo(jurassic_park, 1993, 127).
+releaseInfo(fences, 2016, 55).
+releaseInfo(past_lives, 2023, 120).
+releaseInfo(the_departed, 2006, 151).
+releaseInfo(infinite_journeys, 2025, 140).
+releaseInfo(sinners, 2025, 137).
+releaseInfo(stories_we_tell, 2012, 168).
+releaseInfo(glass_onion, 2022, 139).
+releaseInfo(the_rogue_warrior, 1980, 110).
+releaseInfo(quantum_escape, 2025, 125).
+releaseInfo(shadow_protocol, 2025, 132).
+releaseInfo(stellar_frontier, 2017, 128).
+releaseInfo(neon_chase, 2017, 110).
+releaseInfo(ghost_haven, 2017, 102).
+releaseInfo(astoria_dreams, 2025, 140). 
+
+% --- Directed by ---
+directedBy(unforgiven, clint_eastwood).
+directedBy(gran_torino, clint_eastwood).
+directedBy(minority_report, steven_spielberg).
+directedBy(superman_returns, bryan_singer).
+directedBy(jaws, steven_spielberg).
+directedBy(mad_max_fury_road, george_miller).
+directedBy(300, zack_snyder).
+directedBy(alien_covenant, ridley_scott).
+directedBy(atomic_blonde, david_leitch).
+directedBy(it, andy_muschietti).
+directedBy(jurassic_park, steven_spielberg).
+directedBy(fences, denzel_washington).
+directedBy(past_lives, bong_joon_ho).
+directedBy(the_departed, martin_scorscese).
+directedBy(infinite_journeys, paul_thomas_anderson).
+directedBy(sinners, ryan_coogler).
+directedBy(stories_we_tell, sarah_polley).
+directedBy(glass_onion, rian_johnson).
+directedBy(the_rogue_warrior, john_mctiernan).
+directedBy(quantum_escape, amelia_rivers).
+directedBy(shadow_protocol, darius_mitchell).
+directedBy(stellar_frontier, amelia_rivers).
+directedBy(neon_chase, darius_mitchell).
+directedBy(ghost_haven, andrew_miller).
+directedBy(astoria_dreams, pt_anderson).
+
+% --- Acting information ---
+actedIn(clint_eastwood, unforgiven, william_munny).
+actedIn(morgan_freeman, unforgiven, ned_logan).
+actedIn(clint_eastwood, gran_torino, walt_kowalski).
+actedIn(bee_vang, gran_torino, thao_lor).
+actedIn(tom_cruise, minority_report, john_anderton).
+actedIn(colin_farrell, minority_report, leo_cruz).
+actedIn(kevin_spacey, superman_returns, lex_luthor).
+actedIn(brandon_routh, superman_returns, superman).
+actedIn(roy_schieder, jaws, martin_brody).
+actedIn(richard_dreyfuss, jaws, matt_hooper).
+actedIn(tom_hardy, mad_max_fury_road, max_rockatansky).
+actedIn(charlize_theron, mad_max_fury_road, imperator_furiosa).
+actedIn(gerard_butler, 300, king_leonidas).
+actedIn(lena_headey, 300, queen_gorgo).
+actedIn(michael_fassbender, alien_covenant, david).
+actedIn(katherine_waterston, alien_covenant, daniel).
+actedIn(charlize_theron, atomic_blonde, lorraine_broughton).
+actedIn(james_mcavoy, atomic_blonde, david_perrin).
+actedIn(bill_skaarsgard, it, pennywise).
+actedIn(jaeden_martell, it, bill_denbrough).
+actedIn(sam_neill, jurassic_park, alan_grant).
+actedIn(laura_dern, jurassic_park, ellie_sattler).
+actedIn(denzel_washington, fences, troy_maxson).
+actedIn(viola_davis, fences, rose_maxson).
+actedIn(greta_lee, past_lives, nam_june).
+actedIn(john_cho, past_lives, sae_hyeon).
+actedIn(leonardo_dicaprio, the_departed, billy_costigan).
+actedIn(jack_nicholson, the_departed, frank_costello).
+actedIn(emma_stone, infinite_journeys, ella_summers).
+actedIn(ryan_gosling, infinite_journeys, matthew_turner).
+actedIn(miles_caton, sinners, sammie_moore).
+actedIn(michael_b_jordan, sinners, smoke).
+actedIn(michael_polley, stories_we_tell, storyteller).
+actedIn(susy_buchan, stories_we_tell, storyteller).
+actedIn(janelle_monae, glass_onion, pauline).
+actedIn(daniel_cruz, glass_onion, detective_benoit).
+actedIn(arnold_schwarzenegger, the_rogue_warrior, jack_raven).
+actedIn(sylvester_stallone, the_rogue_warrior, mike_steel).
+actedIn(olivia_cooke, quantum_escape, samantha_ryan).
+actedIn(john_boyega, quantum_escape, alex_keller).
+actedIn(michael_b_jordan, shadow_protocol, jake_ryan).
+actedIn(tessa_thompson, shadow_protocol, claire_evans).
+actedIn(scarlett_johansson, stellar_frontier, captain_lynx).
+actedIn(john_boyega, stellar_frontier, pilot_kade).
+actedIn(zoe_saldana, neon_chase, raven).
+actedIn(oscar_isaac, neon_chase, dex).
+actedIn(emily_browning, ghost_haven, lila).
+actedIn(daniel_radcliffe, ghost_haven, thomas).
+actedIn(brad_pitt, astoria_dreams, danny_swan).
+actedIn(scarlett_johansson, astoria_dreams, lila_moray).
+actedIn(tony_collette, astoria_dreams, margaret_kane).
+
+% --- Movie genres ---
+movieGenre(unforgiven, western).
+movieGenre(gran_torino, drama).
+movieGenre(minority_report, scifi).
+movieGenre(superman_returns, action).
+movieGenre(superman_returns, superhero).
+movieGenre(jaws, thriller).
+movieGenre(jaws, horror).
+movieGenre(mad_max_fury_road, action).
+movieGenre(300, action).
+movieGenre(alien_covenant, scifi).
+movieGenre(atomic_blonde, action).
+movieGenre(it, horror).
+movieGenre(jurassic_park, scifi).
+movieGenre(jurassic_park, horror).
+movieGenre(fences, drama).
+movieGenre(past_lives, drama).
+movieGenre(the_departed, crime).
+movieGenre(the_departed, thriller).
+movieGenre(infinite_journeys, drama).
+movieGenre(infinite_journeys, scifi).
+movieGenre(sinners, drama).
+movieGenre(stories_we_tell, documentary).
+movieGenre(glass_onion, mystery).
+movieGenre(glass_onion, comedy).
+movieGenre(the_rogue_warrior, action).
+movieGenre(the_rogue_warrior, thriller).
+movieGenre(quantum_escape, scifi).
+movieGenre(quantum_escape, thriller).
+movieGenre(shadow_protocol, action).
+movieGenre(shadow_protocol, scifi).
+movieGenre(stellar_frontier, scifi).
+movieGenre(stellar_frontier, action).
+movieGenre(neon_chase, action).
+movieGenre(neon_chase, thriller).
+movieGenre(ghost_haven, horror).
+movieGenre(ghost_haven, mystery).
+movieGenre(astoria_dreams, drama).
+movieGenre(astoria_dreams, thriller).
